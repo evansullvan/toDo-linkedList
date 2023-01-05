5f3871ee32e5519294a890e2ba4bb50517265e29
evan <evansullivan74@gmail.com>
2023-01-05 15:52:28 +0000

    initial commit

---
+++ src/com/company/Node.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Node {
+
+    toDo data;
+    Node next;
+}
---
+++ src/com/company/toDo.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class toDo {
+
+     String title;
+    String desc;
+    boolean isCompleted;
+
+}
